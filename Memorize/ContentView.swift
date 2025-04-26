75b9cfb4966162ac2f23abf171370200561550eb
sjx <2020635197@qq.com>
2025-04-26 15:49:22 +0800

    1549

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -59,7 +59,7 @@ struct ContentView: View { // 这里的View并不是类型，而是协议
         }.foregroundColor(.orange)
     }
     
-    // some changes... 
+    // some changes...
     
     var themeChoosers: some View {
         HStack{
@@ -97,7 +97,7 @@ struct ContentView: View { // 这里的View并不是类型，而是协议
         .imageScale(.large)
         .font(.largeTitle)
     }
-    
+     
     func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button(action: {
             cardCount += offset
